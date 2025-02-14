5a216229b7389cafe81f82236caa73d27829e825
alhiefikri <alhiepedia@gmail.com>
2025-02-14 10:04:22 +0800

    feat: adding caching and ISR in cabinList and cabinPage also adding cabin length in aboutPage

--- app/_components/CabinList.js
+++ app/_components/CabinList.js
@@ -1,7 +1,13 @@
+import { unstable_noStore as noStore } from "next/cache";
+
 import { getCabins } from "../_lib/data-service";
 import CabinCard from "@/app/_components/CabinCard";
 
+export const revalidate = 86400;
+
 async function CabinList() {
+  noStore();
+
   const cabins = await getCabins();
 
   if (!cabins.length) return null;
